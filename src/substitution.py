4dc5453ab276306d0fa4a192a43881b016881490
dxt99 <ericlouis19x@gmail.com>
2023-01-25 23:57:24 +0700

    feat: substitution cipher cracked

--- src/substitution.py
+++ src/substitution.py
@@ -5,6 +5,11 @@ ct = f.read().strip()
 ct = "".join(ct.split("\n"))
 f.close()
 
+def write_file(iteration, buffer):
+    f = open(f"plaintext/substitution/iteration_{iteration}.flag", "w+")
+    f.write(buffer)
+    f.close()
+
 def substitute(ciphertext, dictionary):
     plaintext = ""
     for c in ciphertext:
@@ -16,12 +21,37 @@ def substitute(ciphertext, dictionary):
 
 charset = string.ascii_uppercase
 
-# calculate single letter frequency
-freq = dict.fromkeys(list(charset), 0)
-for c in ct:
-    freq[c] += 1
-freq = dict(sorted(freq.items(), key = lambda x: x[1], reverse = True))
-print(freq)
+def freq_count(ciphertext, seq_len = 1):
+    freq = {}
+    for i in range(0, len(ciphertext)-seq_len+1):
+        key = ciphertext[i:i+seq_len]
+        if key in freq:
+            freq[key] += 1
+        else:
+            freq[key] = 1
+    freq = dict(sorted(freq.items(), key=lambda x: x[1], reverse=True)[:10])
+    return freq
 
-# calculate bigrams frequenct
-# TODO
+# calculate single letter frequency
+print("Single letters:")
+print(freq_count(ct, 1))
+print("Bigrams:")
+print(freq_count(ct, 2))
+print("Trigrams:")
+print(freq_count(ct, 3))
+# try simple decryption
+key = {"W":"e", "C":"t", "Z":"h", "N":"i", "P":"g", "J":"n", "F":"a", "Y":"d"}
+write_file(1, substitute(ct, key))
+# more infers from trigrams and partial decryption
+key.update({"K":"r", "X":"o"})
+write_file(2, substitute(ct, key))
+# more infers
+key.update({"Q":"w", "H":"u", "V":"l"})
+write_file(3, substitute(ct, key))
+
+key.update({"L":"s", "U":"v", "V":"l", "O":"b", "R":'y', "E":"m", "S":"k", "A":'c'})
+write_file(4, substitute(ct, key))
+
+key.update({"B":'f', "G":"p", "I":'x', "D":"z", "M":"j", "T":"q"})
+
+write_file(5, substitute(ct, key))
