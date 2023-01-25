528be2266da4035ed307896b56d2f2c2a8cb8064
dxt99 <ericlouis19x@gmail.com>
2023-01-25 18:00:09 +0700

    feat: single letter freq analysis on subs cipher

--- src/substitution.py
+++ src/substitution.py
@@ -5,10 +5,24 @@ ct = f.read().strip()
 ct = "".join(ct.split("\n"))
 f.close()
 
+def substitute(ciphertext, dictionary):
+    plaintext = ""
+    for c in ciphertext:
+        if c in dictionary:
+            plaintext += dictionary[c]
+        else:
+            plaintext += c
+    return plaintext
+
 charset = string.ascii_uppercase
+
+# calculate single letter frequency
 freq = dict.fromkeys(list(charset), 0)
 for c in ct:
     if c in charset:
         freq[c] += 1
 freq = dict(sorted(freq.items(), key = lambda x: x[1], reverse = True))
 print(freq)
+
+# calculate bigrams frequenct
+# TODO
