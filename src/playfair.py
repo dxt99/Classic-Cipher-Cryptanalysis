ae0dd3ea2019ef7f461af88c1bfa1ad9e937997c
dxt99 <ericlouis19x@gmail.com>
2023-01-27 12:47:00 +0700

    feat: playfair initial guess

--- src/playfair.py
+++ src/playfair.py
@@ -5,6 +5,21 @@ ct = f.read().strip()
 ct = "".join(ct.split("\n"))
 f.close()
 
+def write_file(iteration, buffer):
+    f = open(f"plaintext/playfair/iteration_{iteration}.flag", "w+")
+    f.write(buffer)
+    f.close()
+
+def substitute(ciphertext, dictionary):
+    plaintext = ""
+    for i in range(0, len(ciphertext), 2):
+        c = ciphertext[i: i+2]
+        if c in dictionary:
+            plaintext += dictionary[c]
+        else:
+            plaintext += c
+    return plaintext
+
 def freq_count(ciphertext, seq_len = 2):
     freq = {}
     for i in range(0, len(ciphertext)-seq_len+1):
@@ -17,3 +32,5 @@ def freq_count(ciphertext, seq_len = 2):
     return freq
 
 print(freq_count(ct))
+
+write_file(0, substitute(ct, {"BL":"th", "SH":"he"}))
